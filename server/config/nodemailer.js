295eec39e7d26e2024c15869a9d110a4dbe9de27
Rahul Bose <rahul.basu2005@gmail.com>
2026-09-20 19:25:19 +0530

    Update Brevo API key in nodemailer configuration
    Replaced environment variable for Brevo API key with a hardcoded value.

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,7 +1,7 @@
 const { BrevoClient } = require("@getbrevo/brevo");
 
 const brevo = new BrevoClient({
-    apiKey: process.env.BREVO_API_KEY,
+    apiKey: e3f9e7e12a0e942405c67a90a620de097b5ddc08ec94ea019e0d8d5be29e98a7,
 });
 
 module.exports = brevo;
