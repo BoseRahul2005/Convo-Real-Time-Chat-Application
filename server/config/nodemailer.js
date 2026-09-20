0a28da425948e266423dca75f2e6b3fb254b6512
Rahul Bose <rahul.basu2005@gmail.com>
2026-09-20 19:27:22 +0530

    Replace hardcoded API key with environment variable

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,7 +1,7 @@
 const { BrevoClient } = require("@getbrevo/brevo");
 
 const brevo = new BrevoClient({
-    apiKey: e3f9e7e12a0e942405c67a90a620de097b5ddc08ec94ea019e0d8d5be29e98a7,
+    apiKey: process.env.BREVO_API_KEY,
 });
 
 module.exports = brevo;
